253f4f7d6495b5af44071232582ce91df5fac908
Thibaut Genet <94045978+thibautgnt@users.noreply.github.com>
2024-12-01 21:03:04 +0100

    Test Postman

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-console.log("test from node");
-console.log("test from node", Math.random(), new Date())
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        msg: "hello from API"
+    })
+})
+
+app.post("/", (req, res) => {
+    res.json({
+        msg: "ici le post !!!",
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Serveur en cours d'exécution sur http://localhost:${port}`);
+});
+
