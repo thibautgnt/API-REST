c181e69d6b925fb304687ab3ffbb97199b20de07
Thibaut Genet <94045978+thibautgnt@users.noreply.github.com>
2024-12-01 21:07:48 +0100

    Ajout de la route GET pour récupérer tous les utilisateurs

--- index.js
+++ index.js
@@ -3,17 +3,20 @@ const app = express();
 const port = 3000;
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.json({
-        msg: "hello from API"
-    })
-})
+const users = [
+    { id: 1, firstName: "John", lastName: "Doe", role: "admin" },
+    { id: 2, firstName: "Jane", lastName: "Smith", role: "user" },
+    { id: 3, firstName: "Alice", lastName: "Johnson", role: "moderator" },
+    { id: 4, firstName: "Bob", lastName: "Brown", role: "user" },
+    { id: 5, firstName: "Charlie", lastName: "Davis", role: "admin" },
+]
 
-app.post("/", (req, res) => {
-    res.json({
-        msg: "ici le post !!!",
-    })
-})
+// GET : LIRE tous les utilisateurs
+app.get("/", (req, res) => {
+    res.json(users)
+});
+
+
 
 app.listen(port, () => {
     console.log(`Serveur en cours d'exécution sur http://localhost:${port}`);
